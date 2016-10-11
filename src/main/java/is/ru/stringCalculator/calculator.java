fd074d76e7a9077879b5e23d81ce6f3003cc5db3
FanneyA <fanney13@ru.is>
2016-10-11 23:04:57 +0000

    nýtt fall sem skilar 0 og 1

--- src/main/java/is/ru/stringCalculator/calculator.java
+++ src/main/java/is/ru/stringCalculator/calculator.java
@@ -3,8 +3,11 @@ package is.ru.stringCalculator;
 public class calculator {
 
 	public static int add(String text){
-			
-				return 0;
+		if(text.equals(""))
+			return 0;
+		else
+			return 1;
 	}
+
 }
 	
